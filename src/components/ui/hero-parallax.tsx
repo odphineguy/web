ca40ae33c6383ed8490484f133321247475af51d
Abraham Perez <150524951+odphineguy@users.noreply.github.com>
2025-10-01 04:16:26 -0700

    Increase Hero section text size and change Ultimate to orange

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -98,10 +98,10 @@ export const HeroParallax = ({
 export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The <span className="text-primary">Ultimate</span> <br /> development studio
+      <h1 className="text-4xl md:text-8xl lg:text-9xl font-bold dark:text-white">
+        The <span className="text-orange-500">Ultimate</span> <br /> development studio
       </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+      <p className="max-w-2xl text-lg md:text-2xl mt-8 dark:text-neutral-200">
         We build beautiful products with the latest technologies and frameworks.
         We are a team of passionate developers and designers that love to build
         amazing products.
